e98c8b39cc12307e031897f568a5dd16920261c9
victor <vgil@zymvol.com>
2019-05-10 17:37:59 +0200

    Added CORS Support

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,3 +31,7 @@ RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 # Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
 # individual items  (defaults to read-only item access).
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+# Cross Request
+X_DOMAINS = '*'
+X_HEADERS = ['Authorization', 'Content-type']
