e720b3d7d0ace559ad36aed632a266a667b62fca
victor <vgil@zymvol.com>
2019-05-17 18:24:36 +0200

    Cross request problems solved

--- app/app/main.py
+++ app/app/main.py
@@ -3,6 +3,15 @@ from eve import Eve
 
 app = Eve(__name__, settings="/app/app/settings.py") #"#os.path.join("app", os.path.abspath('settings.py')))
 
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, port=80)
 
